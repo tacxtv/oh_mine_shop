2231f77bb423baf5dde36b440459e9bf012f53be
Tacx <12997062+tacxou@users.noreply.github.com>
2025-04-25 19:36:38 +0200

    fix(users): ensure handleIntervalWhitelist is called in constructor

--- apps/api/src/core/users/users.service.ts
+++ apps/api/src/core/users/users.service.ts
@@ -34,6 +34,7 @@ export class UsersService {
     @InjectRedis() private readonly _redis: Redis,
     @InjectRcon() private _rcon: Rcon,
   ) {
+    this.handleIntervalWhitelist()
     this.logger.verbose('UsersService initialized 🔴')
   }
 
